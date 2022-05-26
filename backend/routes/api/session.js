479accdf4eeafea6fda4d3dd6e7c235492312b91
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-05-26 15:46:22 -0700

    completed logi validation and testing of it

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,14 +3,29 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler'); // will wrap async route handlers + custom middlewares
 
+const { check } = require('express-validator'); // check will be used with handleValidationErrors to validate req bodies
+const { handleValidationErrors } = require('../../utils/validation');
+
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
 // Log in
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
