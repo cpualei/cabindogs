f9d19e5e3bd6e3f37a19dc093a4859b0efd34c84
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-05-26 15:55:29 -0700

    removed GET test route in /routes/indexjs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,4 @@ const apiRouter = require('./api'); // require the api routes
 
 router.use('/api', apiRouter); // all URLS of routes will be prefixed with '/api'
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
-
 module.exports = router;
