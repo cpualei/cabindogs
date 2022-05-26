0af76b8578fa86bc43b4ead91c42074fc829bc5a
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-05-26 15:24:22 -0700

    completed session user api route and testing of it

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,40 +10,53 @@ const router = express.Router();
 
 // Log in
 router.post(
-    '/',
-    asyncHandler(async (req, res, next) => {
-      const { credential, password } = req.body;
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
 
-      const user = await User.login({ credential, password });
+    const user = await User.login({ credential, password });
 
-      // if there is no user returned from the login static method,
-        // create an error and invoke next error-handling middleware
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
+    // if there is no user returned from the login static method,
+      // create an error and invoke next error-handling middleware
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
 
-      // if there is a user returned from the login static method
-        // from the User model, call setTokenCookie and return JSON reponse w/ user info
-      await setTokenCookie(res, user);
+    // if there is a user returned from the login static method
+      // from the User model, call setTokenCookie and return JSON reponse w/ user info
+    await setTokenCookie(res, user);
 
-      return res.json({
-        user
-      });
-    })
-  );
+    return res.json({
+      user
+    });
+  })
+);
 
 // Log out
 router.delete(
-    '/',
-    (_req, res) => {
-      res.clearCookie('token');
-      return res.json({ message: 'success' });
-    }
-  );
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
 
 module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -12,20 +12,20 @@ const router = express.Router();
 router.post(
     '/',
     asyncHandler(async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
 
-      // if the user is successfully created,
+        // if the user is successfully created,
         // call the setTokenCookie and return the JSON res with user info
         // if not, a sequelize validation error will be passed onto next
         // error handling middleware
-      await setTokenCookie(res, user);
+        await setTokenCookie(res, user);
 
-      return res.json({
-        user
-      });
-    })
-  );
+        return res.json({
+            user
+        });
+  })
+);
 
 
 module.exports = router;
