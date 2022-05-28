9efe69cdbaaee863fb2e593ec26ba4d526df1bdf
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-05-28 12:31:54 -0700

    completed bookings model file

--- backend/db/models/bookings.js
+++ backend/db/models/bookings.js
@@ -1,11 +1,28 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Bookings = sequelize.define('Bookings', {
-    userId: DataTypes.INTEGER,
-    listingId: DataTypes.INTEGER,
-    totalCost: DataTypes.INTEGER,
-    startDate: DataTypes.DATE,
-    endDate: DataTypes.DATE
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'Users' }
+    },
+    listingId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: 'Listings' }
+    },
+    totalCost: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
   }, {});
   Bookings.associate = function(models) {
     // associations can be defined here
