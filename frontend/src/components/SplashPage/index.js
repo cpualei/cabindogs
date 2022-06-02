d59d71e1d1574012720cbf735f309cfa7e9f1a1b
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-02 10:12:32 -0700

    made more css changes to splash page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,13 +3,13 @@ import "./SplashPage.css";
 const SplashPage = () => {
   return (
     <div>
-      <div>
+      <div className="h1-div">
         <h1 id="h1">Find yourself outside.</h1>
       </div>
-      <div>
+      <div className="h3-div">
         <h3 id="h3">
-          Discover and book tent camping, RV parks, cabins, treehouses, and
-          glamping.
+          Discover and book tent camping, RV <br></br>
+          parks, cabins, treehouses, and glamping.
         </h3>
       </div>
     </div>
