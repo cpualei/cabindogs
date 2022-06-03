59304c812315018c9862a45a4ecf7c8d81e536d1
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-03 01:21:25 -0700

    added another splash photo to splash page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -12,9 +12,18 @@ const SplashPage = () => {
           Discover and book tent camping, RV <br></br>
           parks, cabins, treehouses, and glamping.
         </h3>
-      <div className="boy-girl-dog-img-div">
-        <img id="boy-girl-dog-img" src="https://www.purina.co.uk/sites/default/files/2020-12/Why%20are%20dogs%20so%20loyalTEASER.jpg"></img>
       </div>
+      <div className="boy-girl-dog-img-div">
+        <img
+          id="boy-girl-dog-img"
+          src="https://www.purina.co.uk/sites/default/files/2020-12/Why%20are%20dogs%20so%20loyalTEASER.jpg"
+        ></img>
+      </div>
+      <div className="snowy-cabin-img-div">
+        <img
+          id="snowy-cabin-img"
+          src="https://newenglandwithlove.com/wp-content/uploads/2020/08/f69c2d9b.f10.webp"
+        ></img>
       </div>
     </div>
   );
