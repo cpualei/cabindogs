5212ddf8a0e6aff4efb216086f99271cfdae0b62
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-03 14:42:20 -0700

    delete feature works. changes made to backend route, thunk, reducer and component

--- frontend/src/components/AllBookingsPage/index.js
+++ frontend/src/components/AllBookingsPage/index.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom";
+import { NavLink, useHistory, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getBookings } from "../../store/bookings";
+import { getBookings, deleteBooking } from "../../store/bookings";
 import { getListings } from "../../store/listings";
 
 const AllBookings = () => {
-  const bookings = useSelector((state) => {
-    return Object.values(state.bookings);
-  });
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  // let bookingsArr = useSelector((state) => {
+  //   return Object.values(state.bookings);
+  // });
+
+  const bookings = useSelector((state) => state.bookings)
+  const bookingsArr = Object.values(bookings)
 
   const listings = useSelector((state) => state.listings);
 
-  console.log(bookings);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -19,14 +25,33 @@ const AllBookings = () => {
     dispatch(getListings());
   }, [dispatch]);
 
+  const handleDeleteClick = (e) => {
+    // dispatch(deleteBooking(booking))
+    history.push("/bookings")
+  }
+
+  // const startDateObj = new Date({booking.startDate}).getMonth
+  // const startDay = startDateObj.getDay
+  // const startMonth = startDateObj.getMonth
+
   return (
     <div>
-      <div></div>
+      <div>Upcoming Reservations</div>
       <ul>
         {listings &&
-          bookings.map((booking) => (
+          bookingsArr.map((booking) => (
             <li key={booking.id}>
+              <div>{booking.startDate} to {booking.endDate}</div>
+
+              <NavLink to={`/bookings/`}>
               <img src={listings[booking.listingId]?.img1}></img>
+              </NavLink>
+              <div>{listings[booking.listingId]?.name}</div>
+              <div>{listings[booking.listingId]?.state}</div>
+              <div>{listings[booking.listingId]?.country}</div>
+              <div>Total Paid: ${listings[booking.listingId]?.cost}</div>
+              <button>Edit Booking Dates</button>
+              <button onClick={(e) => dispatch(deleteBooking(booking))}>Cancel Booking</button>
             </li>
           ))}
       </ul>
