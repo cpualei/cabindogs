5520f5431da7944881396a729fed041c9c094c55
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-06 00:47:46 -0700

    made changes to splashpage and final edits on listing details page

--- frontend/src/components/EditListing/index.js
+++ frontend/src/components/EditListing/index.js
@@ -8,7 +8,7 @@ function EditListingFormModal() {
 
     return (
       <>
-        <button onClick={() => setShowModal(true)}>Edit Listing</button>
+        <button id="listing-details-btns" onClick={() => setShowModal(true)}>Edit Listing</button>
         {showModal && (
           <Modal onClose={() => setShowModal(false)}>
             <EditListingForm setShowModal={setShowModal}/>
