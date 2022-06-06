66c5bdad1363de6409c6c1149e6ab83e13f48593
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-05 17:09:53 -0700

    completed css for listings page

--- frontend/src/components/AllListings/index.js
+++ frontend/src/components/AllListings/index.js
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getListings } from "../../store/listings";
-import "./AllListings.css"
-// import "../images/map"
+import "./AllListings.css";
 
 const AllListings = () => {
   const listings = useSelector((state) => {
@@ -17,19 +16,29 @@ const AllListings = () => {
   }, [dispatch]);
 
   return (
-    <div className="listings-div">
-      <ul className="listings-ul">
-        {listings.map((listing) => (
-          <li id="li" key={listing.id}>
-            <NavLink to={`/listings/${listing.id}`}>
-            <img id="listing-imgs" src={listing.img1} alt="img"></img>
-            </NavLink>
-            <div className="listing-description" id="listing-name">{listing.name}</div>
-            <div className="listing-description">{listing.state}, {listing.country}</div>
-            <div className="listing-description" id="listing-cost">From ${listing.cost} / night</div>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1 id="title">Find your favorite cabin.</h1>
+      <p id="sub-title">Bring your furry friend too.</p>
+      <div className="listings-div">
+        <ul className="listings-ul">
+          {listings.map((listing) => (
+            <li id="li" key={listing.id}>
+              <NavLink to={`/listings/${listing.id}`}>
+                <img id="listing-imgs" src={listing.img1} alt="img"></img>
+              </NavLink>
+              <div className="listing-description" id="listing-name">
+                {listing.name}
+              </div>
+              <div className="listing-description">
+                {listing.state}, {listing.country}
+              </div>
+              <div className="listing-description" id="listing-cost">
+                From ${listing.cost} / night
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
