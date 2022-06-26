9a8ca7292e7a4ee16bb0460ac16cacfefb9f5699
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-06-25 20:48:44 -0700

    added background to login page

--- frontend/src/components/LoginForm/index.js
+++ frontend/src/components/LoginForm/index.js
@@ -39,62 +39,80 @@ function LoginFormPage() {
   };
 
   return (
-    <div>
-      <h1 id="login-title">Welcome back!</h1>
-      <p id="sub-title">Let's get you and your pals outside.</p>
+    <>
+      <div
+        id="bkgrnd-img"
+        style={{
+          backgroundImage: `url(https://www.chaletvillage.com/wp-content/uploads/2017/09/shutterstock_512245666.jpg)`,
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div id="login-form-outer-container">
+          <div id="login-form-container">
+            <h1 id="login-title">Welcome back!</h1>
+            <p id="sub-title">Let's get you and your pals outside.</p>
 
-      <div id="login-form-div">
-        <form id="login-form" onSubmit={handleSubmit}>
-          <ul id="login-ul">
-            {errors.map((error, idx) => (
-              <li key={idx}>{error}</li>
-            ))}
-          </ul>
-          <div id="labels-inputs-btns-div">
-            <div id="labels-inputs-div">
-              <label className="user-email-label-input">
-                Username or Email
-                <input
-                  className="user-email-label-input"
-                  type="text"
-                  value={credential}
-                  onChange={(e) => setCredential(e.target.value)}
-                  required
-                />
-              </label>
+            <div id="login-form-div">
+              <form id="login-form" onSubmit={handleSubmit}>
+                <ul id="login-ul">
+                  {errors.map((error, idx) => (
+                    <li key={idx}>{error}</li>
+                  ))}
+                </ul>
+                <div id="labels-inputs-btns-div">
+                  <div id="labels-inputs-div">
+                    <label className="user-email-label-input">
+                      Username or Email
+                      <input
+                        className="user-email-label-input"
+                        type="text"
+                        value={credential}
+                        onChange={(e) => setCredential(e.target.value)}
+                        required
+                      />
+                    </label>
 
-              <label className="user-email-label-input">
-                Password
-                <input
-                  className="user-email-label-input"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </label>
+                    <label className="user-email-label-input">
+                      Password
+                      <input
+                        className="user-email-label-input"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </label>
+                  </div>
+                  <div id="btns-div">
+                    <button className="login-and-demo-btns" type="submit">
+                      Log in
+                    </button>
+                    <button
+                      className="login-and-demo-btns"
+                      onClick={(e) => handleDefaultButton(e)}
+                    >
+                      Demo User
+                    </button>
+                  </div>
+                  <p id="login-bottom-form-text">
+                    Don't have an account?{" "}
+                    <NavLink
+                      to="/signup"
+                      id="login-bottom-form-link"
+                      style={{ textDecoration: "none" }}
+                    >
+                      Sign up!
+                    </NavLink>
+                  </p>
+                </div>
+              </form>
             </div>
-            <div id="btns-div">
-              <button className="login-and-demo-btns" type="submit">
-                Log in
-              </button>
-              <button
-                className="login-and-demo-btns"
-                onClick={(e) => handleDefaultButton(e)}
-              >
-                Demo User
-              </button>
-            </div>
-            <p id="login-bottom-form-text">
-              Don't have an account?{" "}
-              <NavLink to="/signup" id="login-bottom-form-link" style={{ textDecoration: "none" }}>
-                Sign up!
-              </NavLink>
-            </p>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
