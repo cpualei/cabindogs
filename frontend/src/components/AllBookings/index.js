cc1fb30dc68872e0abfb8ffc3b4a001a26a5da73
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-08-11 11:32:13 -1000

    added conditional rendering, if user has no bookings, a different msg will appear

--- frontend/src/components/AllBookings/index.js
+++ frontend/src/components/AllBookings/index.js
@@ -3,8 +3,7 @@ import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getBookings, deleteBooking } from "../../store/bookings";
 import { getListings } from "../../store/listings";
-// import BookingDate from "../BookingDate";
-import "./AllBookings.css"
+import "./AllBookings.css";
 
 const AllBookings = () => {
   const history = useHistory();
@@ -20,45 +19,57 @@ const AllBookings = () => {
     dispatch(getBookings());
   }, [dispatch]);
 
-  // const startDateObj = new Date({booking.startDate}).getMonth
-  // const startDay = startDateObj.getDay
-  // const startMonth = startDateObj.getMonth
-
-
   return (
     <div id="bookings-div">
-      <h1 id="bookings-title">Upcoming Reservations.</h1>
+      {!bookingsArr2.length ? (
+        <h1 id="bookings-title">You have no bookings.</h1>
+      ) : (
+        <h1 id="bookings-title">Upcoming reservations</h1>
+      )}
       <div id="bookings-ul-div">
         <ul id="bookings-ul">
           {listings &&
             bookingsArr2.map((booking) => (
-              <li id="booking-li" key={booking.id} >
-                  {/* <p>{listings[booking.listingId]?.name}</p> */}
-                  <img id="booking-img" src={listings[booking.listingId]?.img1}></img>
-                  <div id="booking-description-div">
-                    <div className="booking-description" id="booking-name">{listings[booking.listingId]?.name}</div>
-                    <div className="booking-description" id="booking-state">{listings[booking.listingId]?.state}, {listings[booking.listingId]?.country}</div>
-                    {/* <div id="booking-description" id="booking-guests">{listings[booking.listingId]?.guests}</div> */}
-                    <div className="booking-description" id="booking-dates">{
+              <li id="booking-li" key={booking.id}>
+                {/* <p>{listings[booking.listingId]?.name}</p> */}
+                <img
+                  id="booking-img"
+                  src={listings[booking.listingId]?.img1}
+                ></img>
+                <div id="booking-description-div">
+                  <div className="booking-description" id="booking-name">
+                    {listings[booking.listingId]?.name}
+                  </div>
+                  <div className="booking-description" id="booking-state">
+                    {listings[booking.listingId]?.state},{" "}
+                    {listings[booking.listingId]?.country}
+                  </div>
+                  {/* <div id="booking-description" id="booking-guests">{listings[booking.listingId]?.guests}</div> */}
+                  <div className="booking-description" id="booking-dates">
+                    {
                       // <BookingDate booking={booking?.startDate} />
                       booking?.startDate
-                    } to {
-                    booking?.endDate
-                    }</div>
-                    <div className="booking-description" id="booking-cost">Total Paid: ${listings[booking?.listingId]?.cost+((listings[booking?.listingId]?.cost)*0.4)}</div>
-                    <div id="delete-btn-div">
-                      <button
-                        id="delete-btn"
-                        onClick={(e) => {
-                          dispatch(deleteBooking(booking));
-                          history.push("/bookings");
-                        }}
-                        >
-                        Cancel Reservation
-                      </button>
-                    </div>
-                    </div>
-                </li>
+                    }{" "}
+                    to {booking?.endDate}
+                  </div>
+                  <div className="booking-description" id="booking-cost">
+                    Total Paid: $
+                    {listings[booking?.listingId]?.cost +
+                      listings[booking?.listingId]?.cost * 0.4}
+                  </div>
+                  <div id="delete-btn-div">
+                    <button
+                      id="delete-btn"
+                      onClick={(e) => {
+                        dispatch(deleteBooking(booking));
+                        history.push("/bookings");
+                      }}
+                    >
+                      Cancel Reservation
+                    </button>
+                  </div>
+                </div>
+              </li>
             ))}
         </ul>
       </div>
