8cd7c01298d4fbbe57f2df7e3b895822473fb441
Caitlin Buen-Lucas <cbuenlucas@gmail.com>
2022-08-15 13:02:25 -1000

    added review model, migrations, and seeder files

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     review: {
-      type: DataTypes.STRING(2200),
+      type: DataTypes.STRING(2000),
       allowNull: false
     }
   }, {});
